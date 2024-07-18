6fe067baa4a9a2ffa737dd2d7a94eccfdc136bbc
vietkynl99 <vietdohoang1999@gmail.com>
2024-07-18 22:18:19 +0700

    Add source code

---
+++ VLog.h
@@ -0,0 +1,23 @@
+#ifndef _VLOG_H_
+#define _VLOG_H_
+
+#include <Arduino.h>
+
+#define VLOG(...) VLog::print(TAG, __VA_ARGS__);
+#define VLOG_NO_TAG(...) VLog::print(nullptr, __VA_ARGS__);
+
+class VLog
+{
+public:
+    static void print(const char *tag, const char *pFormat, ...)
+    {
+        static char mStringBuffer[150];
+        va_list pVlist;
+        va_start(pVlist, pFormat);
+        vsnprintf(mStringBuffer, sizeof(mStringBuffer) - 1, pFormat, pVlist);
+        va_end(pVlist);
+
+        Serial.println(mStringBuffer);
+    }
+};
+#endif
