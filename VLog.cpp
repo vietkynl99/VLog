5183ad9f8e0a35b25d6f606415596ccd6a37ae1b
vietkynl99 <vietdohoang1999@gmail.com>
2024-08-01 15:08:19 +0700

    Allow to choose debug Serial

---
+++ VLog.cpp
@@ -0,0 +1,34 @@
+#include "VLog.h"
+
+Stream *VLog::mSerial = &Serial;
+
+void VLog::init(Stream *serial)
+{
+    mSerial = serial;
+}
+
+void VLog::print(bool newline, const char *tag, const char *pFormat, ...)
+{
+    if (mSerial == nullptr)
+    {
+        return;
+    }
+
+    static char mStringBuffer[150];
+    va_list pVlist;
+    va_start(pVlist, pFormat);
+    vsnprintf(mStringBuffer, sizeof(mStringBuffer) - 1, pFormat, pVlist);
+    va_end(pVlist);
+
+    if (tag)
+    {
+        mSerial->print('[');
+        mSerial->print(tag);
+        mSerial->print(']');
+    }
+    mSerial->print(mStringBuffer);
+    if (newline)
+    {
+        mSerial->println();
+    }
+}
--- VLog.h
+++ VLog.h
@@ -3,32 +3,17 @@
 
 #include <Arduino.h>
 
-#define VLOG_SERIAL         Serial
-
-#define VLOG_PRINT(...)     VLOG_SERIAL.print(__VA_ARGS__);
-#define VLOG_PRINTLN(...)   VLOG_SERIAL.println(__VA_ARGS__);
-
-#define VLOG(...)           VLog::print(TAG, __VA_ARGS__);
-#define VLOG_NO_TAG(...)    VLog::print(nullptr, __VA_ARGS__);
+#define VLOG_PRINT(...)     VLog::print(false, nullptr, __VA_ARGS__);
+#define VLOG_PRINTLN(...)   VLog::print(true, nullptr, __VA_ARGS__);
+#define VLOG(...)           VLog::print(true, TAG, __VA_ARGS__);
 
 class VLog
 {
-public:
-    static void print(const char *tag, const char *pFormat, ...)
-    {
-        static char mStringBuffer[150];
-        va_list pVlist;
-        va_start(pVlist, pFormat);
-        vsnprintf(mStringBuffer, sizeof(mStringBuffer) - 1, pFormat, pVlist);
-        va_end(pVlist);
+private:
+    static Stream *mSerial;
 
-        if (tag)
-        {
-            VLOG_SERIAL.print('[');
-            VLOG_SERIAL.print(tag);
-            VLOG_SERIAL.print(']');
-        }
-        VLOG_SERIAL.println(mStringBuffer);
-    }
+public:
+    static void init(Stream *serial);
+    static void print(bool newline, const char *tag, const char *pFormat, ...);
 };
 #endif
