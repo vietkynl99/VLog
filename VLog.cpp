cd486b16f15ea9a434e37636019c9a61cb3e9eee
vietkynl99 <vietdohoang1999@gmail.com>
2024-09-12 23:35:55 +0700

    Support multiple streams

--- VLog.cpp
+++ VLog.cpp
@@ -1,33 +1,58 @@
 #include "VLog.h"
 
-Stream *VLog::mSerial = &Serial;
+VVector<Stream *> *VLog::mStreamList = nullptr;
 
 void VLog::init(Stream *serial)
 {
-    mSerial = serial;
+    if (mStreamList)
+    {
+        delete mStreamList;
+        mStreamList = nullptr;
+    }
+    mStreamList = new VVector<Stream *>(1);
+    mStreamList->push_back(serial ? serial : &Serial);
+}
+
+void VLog::init(const VVector<Stream *> &streamList)
+{
+    if (mStreamList)
+    {
+        delete mStreamList;
+        mStreamList = nullptr;
+    }
+    mStreamList = new VVector<Stream *>();
+    *mStreamList = streamList;
 }
 
 void VLog::print(bool newline, const char *tag, const __FlashStringHelper *pFormat, ...)
 {
+    if (!mStreamList)
+    {
+        return;
+    }
+
     char mStringBuffer[VLOG_BUFFER_SIZE];
     char pcFormat[VLOG_FORMAT_SIZE];
 
-    strncpy_P(pcFormat, (const char*)pFormat, sizeof(pcFormat));
+    strncpy_P(pcFormat, (const char *)pFormat, sizeof(pcFormat));
 
     va_list pVlist;
     va_start(pVlist, pcFormat);
     vsnprintf(mStringBuffer, sizeof(mStringBuffer) - 1, pcFormat, pVlist);
     va_end(pVlist);
 
-    if (tag)
+    for (int i = 0; i < mStreamList->size(); i++)
     {
-        mSerial->print('[');
-        mSerial->print(tag);
-        mSerial->print("] ");
-    }
-    mSerial->print(mStringBuffer);
-    if (newline)
-    {
-        mSerial->println();
+        if (tag)
+        {
+            mStreamList->at(i)->print('[');
+            mStreamList->at(i)->print(tag);
+            mStreamList->at(i)->print("] ");
+        }
+        mStreamList->at(i)->print(mStringBuffer);
+        if (newline)
+        {
+            mStreamList->at(i)->println();
+        }
     }
 }
--- VLog.h
+++ VLog.h
@@ -2,6 +2,7 @@
 #define _VLOG_H_
 
 #include <Arduino.h>
+#include <VVector.h>
 
 #define VLOG_PRINT(...)     VLog::print(false, nullptr, __VA_ARGS__);
 #define VLOG_PRINTLN(...)   VLog::print(true, nullptr, __VA_ARGS__);
@@ -13,10 +14,11 @@
 class VLog
 {
 private:
-    static Stream *mSerial;
+    static VVector<Stream *> *mStreamList;
 
 public:
-    static void init(Stream *serial);
+    static void init(Stream *serial = nullptr);
+    static void init(const VVector<Stream *> &streamList);
     static void print(bool newline, const char *tag, const __FlashStringHelper *pFormat, ...);
 };
 #endif
